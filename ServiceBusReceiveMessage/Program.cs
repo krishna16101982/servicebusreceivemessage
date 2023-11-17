60712071fbbd4999df96355e18c234ccdddb4af1
Kumar Gaurav <krishna16101982@gmail.com>
2023-11-17 14:56:49 +0530

    first commit

---
+++ ServiceBusReceiveMessage/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System.Text;
+
+class Program
+{
+    const string ServiceBusConnectionString = "Endpoint=sb://azservicebus001.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=KcEVSrHaF5Pcs3ns5TvnJWTdA7s36Wn7K+ASbMmAkGY=";
+    const string QueueName = "servicebusqueue001";
+    static IQueueClient queueClient;
+
+    static async Task Main(string[] args)
+    {
+        queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+        // Register the message handler
+        RegisterOnMessageHandlerAndReceiveMessages();
+
+        Console.ReadKey();
+
+        await queueClient.CloseAsync();
+    }
+
+    static void RegisterOnMessageHandlerAndReceiveMessages()
+    {
+        var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+        {
+            MaxConcurrentCalls = 1,
+            AutoComplete = false
+        };
+
+        queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+    }
+
+    static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+    {
+        Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+        // Process the message
+        // For example, you can add your processing logic here
+
+        await queueClient.CompleteAsync(message.SystemProperties.LockToken); // Complete the message
+    }
+
+    static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+    {
+        Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+        var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+        Console.WriteLine("Exception context for troubleshooting:");
+        Console.WriteLine($"- Endpoint: {context.Endpoint}");
+        Console.WriteLine($"- Entity Path: {context.EntityPath}");
+        Console.WriteLine($"- Executing Action: {context.Action}");
+
+        return Task.CompletedTask;
+    }
+}
